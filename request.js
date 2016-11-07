33d9a0ddf27626b146efbe64daff73bdd203b4ba
Micah Britton <micahwkb@gmail.com>
2016-11-07 15:00:15 -0500

    refined request function

--- request.js
+++ request.js
@@ -2,18 +2,25 @@ var request = require('request');
 var fs = require('fs');
 
 console.log('\nAttempting download...')
-request.get('https://sytantris.github.io/http-examples/future.jpg', function(err, response){
+
+request.get('https://sytantris.github.io/http-examples/future.jpg', function(err, response, data){
          if(err){
            throw err;
          } else if(response.statusCode === 200) {
               console.log('\nDownload finished!')
          }
        })
-        .on('data', function(data) {
-          console.log("Downloading...");
-        })
         .on('response', function(response) {
-         console.log('\nResponse Status Code: ', response.statusCode, '\nResponse Message: ', response.statusMessage, '\nContent Type: ',response.headers['content-type'], '\n');
-
+          console.log('Response Status Code: ', response.statusCode);
+          console.log('Response Message: ', response.statusMessage);
+          console.log('Content Type: ',response.headers['content-type'], '\n');
+          if (response.statusCode === 200) {
+            response.on('data', function(data) {
+            console.log("Downloading...")
+            })
+          }
         })
+        /*.on('data', function(data) {
+          console.log("Downloading...");
+        })*/
         .pipe(fs.createWriteStream('./future.jpg'))
